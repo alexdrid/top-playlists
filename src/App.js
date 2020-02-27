e4f1b84256a425f359f1cf5020769d2425a0f858
= <=>
2020-02-27 03:16:57 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -76,8 +76,8 @@ class Filter extends Component {
     return (
       <div style={defaultStyle}>
         <img />
-        <input type="text" onKeyUp={(event) => 
-          this.props.onTextChange(event.target.value)}/>
+        <input type="text" onKeyUp={(event) =>
+          this.props.onTextChange(event.target.value)} />
       </div>
     );
   }
@@ -109,6 +109,7 @@ class App extends Component {
       filterString: ''
     }
   }
+
   componentDidMount() {
     setTimeout(() => {
       this.setState({ serverData: fakeServerData });
@@ -116,6 +117,11 @@ class App extends Component {
   }
 
   render() {
+    let playlistsToRender = this.state.serverData.user ? this.state.serverData.user.playlists
+      .filter(playlist =>
+        playlist.name.toLowerCase().includes(
+          this.state.filterString.toLowerCase())
+      ) : []
     return (
       <div className="App">
         {this.state.serverData.user ?
@@ -123,8 +129,8 @@ class App extends Component {
             <h1 style={{ ...defaultStyle, 'font-size': '54px' }}>
               {this.state.serverData.user.name}'s Playlists
           </h1>
-            <PlaylistCounter playlists={this.state.serverData.user.playlists} />
-            <HoursCounter playlists={this.state.serverData.user.playlists} />
+            <PlaylistCounter playlists={playlistsToRender} />
+            <HoursCounter playlists={playlistsToRender} />
             <Filter onTextChange={text => this.setState({ filterString: text })} />
             {this.state.serverData.user.playlists.filter(playlist =>
               playlist.name.toLowerCase().includes(this.state.filterString)
